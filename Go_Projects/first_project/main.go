155db2b6c595bf9e4eef4c3abc65ad3cce6b1679
Najma Nahar <154003670+najmaleadgenix@users.noreply.github.com>
2026-06-13 09:06:46 +0600

    Add files via upload

---
+++ Go_Projects/first_project/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main(){
+	// fmt.Println("Hello World!")
+	// a:= 10
+	// fmt.Println(a)
+	// var a int = 10
+	// fmt.Println(a) 
+	a:= 10
+	a=20
+	fmt.Println(a)
+}
